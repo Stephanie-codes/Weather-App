83a446bc3bde6c1fb04c877f410d560f6896bca9
Stephanie-codes <stephanieb170@gmail.com>
2023-08-12 22:59:46 +0100

    css styling added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+
+        <div className="search">
+          <input value="Enter Location" />
+        </div> 
+
+        <div className="middle">
+          <div className="location">
+            <h2>Canary Wharf</h2>
+          </div>
+          <div className="temperature">
+            <h1>28°C</h1>
+          </div>
+          <div className="description">
+            <h3>Sunny</h3>
+          </div>
+          <div className="date">
+            <h4>Sat 12 Aug 22:01</h4>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feel">
+            <p>Feels like:</p> 
+            <p> 26°C</p>
+          </div>
+          <div className="uv">
+            <p>UV Index:</p>
+            <p>Low</p>
+          </div>
+          <div className="wind">
+            <p>Wind Speed:</p>
+            <p>8mph</p>
+          </div>
+          <div className="pollen">
+            <p>Pollen:</p> 
+            <p>Low</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
